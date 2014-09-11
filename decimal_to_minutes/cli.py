f432df7e41a853cff2ea36b2ecb168c4c8b6be23
Jonathan Chu <jonathan.chu@me.com>
2014-09-11 00:52:56 -0400

    Initial commit.

---
+++ decimal_to_minutes/cli.py
@@ -0,0 +1,24 @@
+from decimal import Decimal
+from math import floor
+import sys
+
+import click
+
+
+@click.command()
+@click.argument('t', required=True)
+def main(t):
+    """Converts a decimal to minutes."""
+    time_hours = Decimal(t)
+    time_minutes = time_hours * 60
+    time_seconds = time_minutes * 60
+
+    hours_part = floor(time_hours % 60)
+    minutes_part = floor(time_minutes % 60)
+    seconds_part = floor(time_seconds % 60)
+
+    click.echo("{h}:{m}:{s}".format(
+        h=int(hours_part),
+        m=int(minutes_part),
+        s=int(seconds_part)
+    ))
